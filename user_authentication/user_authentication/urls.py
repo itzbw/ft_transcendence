b73e493e1e576ebb2e72f2156b95143821d93ebf
Gurvan Le Minier <gleminier2@gmail.com>
2024-05-07 00:09:18 +0200

    User authentication works [DOING] ---[fix]: change url function to path and import its modules in user_authentication/urls.py

--- user_authentication/user_authentication/urls.py
+++ user_authentication/user_authentication/urls.py
@@ -1,9 +1,9 @@
 # user_authentication/urls.py
 
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r"^", include("users.urls")),
-    url(r"^admin/", admin.site.urls),
+    path('', include("users.urls")),
+    path('admin/', admin.site.urls),
 ]
