cc2bc6544381572549b6119e415afce1e4ee78b7
Gurvan Le Minier <gleminier2@gmail.com>
2024-05-07 04:32:28 +0200

    User authentication works [DOING] ---[feat]: set EMAIL_HOST and EMAIN_PORT for testing in settings.py

--- user_authentication/user_authentication/settings.py
+++ user_authentication/user_authentication/settings.py
@@ -128,3 +128,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email settings for testing
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
