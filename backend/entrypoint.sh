79dc7c8470ee0168ebdde80d1c898c6b4b0aa154
Stephen ELIE <stelie@student.42.fr>
2024-07-11 15:43:01 +0200

    added back+db not ttally working

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+while true; do
+    if nc -z -w 2 $POSTGRES_HOST $POSTGRES_PORT; then
+        echo "Postgres is up!"
+        break
+    else
+        echo "Postgres isn't up...waiting..."
+        sleep 2
+    fi
+done
