3ecbe44e855102c81d9d1c99331a7caeff1cb223
Stephen ELIE <stelie@student.42.fr>
2024-07-27 13:52:52 +0200

    +save() override to add creation date

--- backend/django/users/models.py
+++ backend/django/users/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 # Create your models here.
 class SiteUser(AbstractUser):
 	username = models.CharField(max_length=30, unique=True)
 	email = models.EmailField(unique=True)
-	avatar = models.ImageField(verbose_name="Profile picture")
+	avatar = models.ImageField(verbose_name="Profile picture", null=True, blank=True)
+	datecreated = models.DateField(null=True, blank=True)
+
+	# Override SiteUser.save() to get the date creation
+	def save(self, *args, **kwargs):
+		if not self.id:  # Si l'utilisateur est nouveau
+			self.datecreated = timezone.now().date()
+		super(SiteUser, self).save(*args, **kwargs)
