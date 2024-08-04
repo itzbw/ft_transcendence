9e018e5dd4042deb7c4d36ec81a36c02c879aeea
Stephen ELIE <stelie@student.42.fr>
2024-08-04 12:28:57 +0200

    cleaned entrypoint

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -1,10 +1,10 @@
-# Install dependencies
+# Wait for the databse to be ready
 while true; do
     if nc -z -w 2 $DB_HOST $DB_PORT; then
-        echo "Postgres is up!"
+        echo "\e[32mPostgres is up!\e[0m"
         break
     else
-        echo "Postgres isn't up...waiting..."
+        echo "\e[33mPostgres isn't up...waiting...\e[0m"
         sleep 2
     fi
 done
@@ -14,23 +14,21 @@ if [ -f /logs/gunicorn.log ]; then
 	echo "\n----------- SERVER LAUNCH ------------\n" >> /logs/gunicorn.log
 fi
 
-# Adds a seperator in error logfile to see when the container is re-created
-if [ -f /logs/gunicorn_error.log ]; then
-	echo "\n----------- SERVER LAUNCH ------------\n" >> /logs/gunicorn_error.log
-fi
-
+echo "\e[33mCreating migration files\e[0m"
 python manage.py makemigrations
+
+echo "\e[33mApplying database migrations\e[0m"
 python manage.py migrate
-# python manage.py runserver 0.0.0.0:$BACK_PORT
 
 # Need to delete that later :
 python manage.py create_superuser
 
 # gunicorn is a WSGI server for Django
+echo "\e[32mLaunching server...\e[0m"
 gunicorn --bind 0.0.0.0:$BACK_PORT \
 			--certfile=$BACK_SSL_CERT \
 			--keyfile=$BACK_SSL_KEY \
 			ft_transcendence.wsgi:application \
 			--log-file /logs/gunicorn.log \
 			--log-level debug
-			# > /logs/gunicorn_stdout.log 2>&1
+
