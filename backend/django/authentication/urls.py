30ab4743fd9f88854ab1c41503c937230933f10d
Alessio Benincasa <albeninc@made-f0Ar2s4.clusters.42paris.fr>
2024-08-12 18:49:48 +0200

    done

--- backend/django/authentication/urls.py
+++ backend/django/authentication/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import LoginView, logout_view, login_status, RegisterView
+from two_factor.views import SetupView
 
 urlpatterns = [
 	path('status/', login_status, name='login-status'),
 	path('register/', RegisterView.as_view(), name='register'),
 	path('login/', LoginView.as_view(), name='login'),
 	path('logout/', logout_view, name='logout'),
+    path('login2/', SetupView.as_view(), name='login2'), #to do to redirect
 ]
