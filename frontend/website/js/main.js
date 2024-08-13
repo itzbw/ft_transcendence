5be3e6c9b41d456c042cf37ce8abbee920559879
Stephen ELIE <stelie@student.42.fr>
2024-08-13 16:01:03 +0200

    removed TEST button

--- frontend/website/js/main.js
+++ frontend/website/js/main.js
@@ -1,43 +1,24 @@
-import { loadContent, addTestButton } from './tools.js';  // ** TO_REMOVE ** addTestButton
-import { locationHandler } from './router.js';
-
 // header
 import { setHeader } from './header/header.js';
-
-
-// // Translation
-// import { applyLanguage} from './language.js';
 
 // authentification
 import { checkLoginStatus } from './auth/status.js';
 import { setupLogin } from './auth/login.js';
 
 
-
-
 // MAIN FUNCTION
 document.addEventListener('DOMContentLoaded', async function() {
 
+	// check if user is authenticated
 	const status = await checkLoginStatus();
 	
+	// load the header
 	await setHeader(status);
 
+	// if not authenticated, load the login page
 	if (!status.isAuthenticated) {
 		console.log("no user authenticated");
 		setupLogin('init');
 	}
-
-
-
-
-
-
-
-
-	// For tests only ** TO_REMOVE **
-	addTestButton();
 });
-
-
-
 
