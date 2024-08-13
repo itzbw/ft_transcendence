ad755ce7c2e051dd6c8a6c30441aa8edec08091c
Alessio Benincasa <albeninc@made-f0Ar3s6.clusters.42paris.fr>
2024-08-13 15:16:12 +0200

    cannot connect but tokens are generated

--- backend/django/authentication/urls.py
+++ backend/django/authentication/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import LoginView, logout_view, login_status, RegisterView
 from two_factor.views import SetupView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
 	path('status/', login_status, name='login-status'),
 	path('register/', RegisterView.as_view(), name='register'),
-	path('login/', LoginView.as_view(), name='login'),
+	path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # Utilisation du JWT pour le login
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 	path('logout/', logout_view, name='logout'),
     path('login2/', SetupView.as_view(), name='login2'), #to do to redirect
 ]
