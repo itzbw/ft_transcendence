3069ef4dd70ba490c5656a5da855c73d817ef8d7
Alessio Benincasa <albeninc@made-f0Cr9s1.clusters.42paris.fr>
2024-08-13 14:40:28 +0200

    JWT implementation began

--- backend/django/ft_transcendence/settings.py
+++ backend/django/ft_transcendence/settings.py
@@ -59,6 +59,26 @@ INSTALLED_APPS = [
     'two_factor.plugins.email',  # <- if you want email capability.
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': 'django-insecure-bq^l6js&*iwr+e&zpvt3toh*66ol1edrh*3m4x@h#jck7sa#^l',
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
