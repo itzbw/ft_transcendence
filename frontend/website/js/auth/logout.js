03dc6241e0ce9fd07681702691d66e7eb7cbf550
bw <thebw.42@pm.me>
2024-08-16 15:53:25 +0200

    fix: switched logout to jwt

--- frontend/website/js/auth/logout.js
+++ frontend/website/js/auth/logout.js
@@ -9,29 +9,8 @@ export function doLogout() {
 		return;
 	}
 
-	// send a disconnection request
-	fetch('/api/authentication/logout/', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-CSRFToken': getCookie('csrftoken'),
-		},
-	})
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response for logout was not ok');
-			}
-			return (response.json());
-		})
-		.then(data => {
-			console.log("User disconnected successfully:", data);
-
-			// redirect to homepage
-			window.location.href = '/';
-			location.reload();
-		})
-		// Error handling
-		.catch(error => console.error('Error:', error));
+	localStorage.clear()
+	location.reload()
 }
 
 export function setupLogout() {
