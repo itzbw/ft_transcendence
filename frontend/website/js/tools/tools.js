600fc373abca7e01b13af8c1cab5d9cbda530974
Stephen ELIE <stelie@student.42.fr>
2024-08-17 16:20:30 +0200

    rm unused functions

--- frontend/website/js/tools/tools.js
+++ frontend/website/js/tools/tools.js
@@ -1,35 +1,3 @@
-
-// // ---------- START executeScript() ---------------- 
-// export function executeScripts(node) {
-// 	if (nodeScriptIs(node) === true) {
-// 		node.parentNode.replaceChild(nodeScriptClone(node), node);
-// 	}
-// 	else {
-// 		var i = -1, children = node.childNodes;
-// 		while (++i < children.length) {
-// 			executeScripts(children[i]);
-// 		}
-// 	}
-
-// 	return node;
-// }
-
-// function nodeScriptClone(node) {
-// 	var script = document.createElement("script");
-// 	script.text = node.innerHTML;
-
-// 	var i = -1, attrs = node.attributes, attr;
-// 	while (++i < attrs.length) {
-// 		script.setAttribute((attr = attrs[i]).name, attr.value);
-// 	}
-// 	return script;
-// }
-
-// function nodeScriptIs(node) {
-// 	return node.tagName === 'SCRIPT';
-// }
-// -------------------------- 
-
 // ---------- loadContent () ---------------- 
 // Load html file's content and insert it in the div:
 export async function loadContent(file, elementID, callback) {
