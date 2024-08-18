ac986672c5df36aaaaf4a43552ebb7dfe7807d03
Loic BASTIAN <lbastian@made-f0Ar3s1.clusters.42paris.fr>
2024-08-18 16:16:26 +0200

    hash when page reloaded

--- frontend/website/js/tools/router.js
+++ frontend/website/js/tools/router.js
@@ -4,9 +4,53 @@ import { setGameModeSelection } from "../game/select_game_mode.js";
 import { loadVsBotGame } from "../game/game_vs_bot.js";
 import { loadVsHumanGame } from "../game/game_vs_human.js";
 
+function checkHash()
+{
+	const hash = window.location.hash;
+
+	switch (true) {
+
+		// profile
+		case hash.startsWith('#profile/'):
+			const prefixLength = '#profile/'.length;
+			const username = hash.substring(prefixLength);
+			showUserProfile(username);
+			break;
+
+		// leaderboard
+		case hash === '#leaderboard':
+			leaderboard();
+			break;
+
+		// game mode selection
+		case hash === '#game-menu':
+			setGameModeSelection();
+			break;
+
+		// pong VS bot
+		case hash === "#pongvsbot":
+			loadVsBotGame();
+			break;
+
+		// pong VS man
+		case hash === "#pongvsman":
+			loadVsHumanGame();
+			break;
+
+		// tournament
+		case hash === "#tournament":
+			break;
+
+		// default
+		default:
+			document.getElementById('main-box').innerHTML = ''; // Default to main menu
+	}
+}
+
 // when hash is changed, will launch the concerned page
 export function router() {
 
+	checkHash()
 	// listen for hashchange event
 	window.addEventListener('hashchange', () => {
 		const hash = window.location.hash;
